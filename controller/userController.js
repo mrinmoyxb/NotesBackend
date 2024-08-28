92c8419947dcb6e8c483d2b50a97730e2469f3aa
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-08-28 18:09:27 +0530

    signup and login added

---
+++ controller/userController.js
@@ -0,0 +1,61 @@
+const User = require("../model/userModel")
+const bcrypt = require("bcrypt")
+
+//* signup
+async function handleSignUp(req, res){
+    try{
+        if(!req.body || !req.body.email || !req.body.password){
+            return res.status(404).json({response: "enter all details"})
+        }
+        if(req.body.password.length < 6){
+            return res.status(400).json({response: "length of password is less than 6"})
+        }
+        else{
+            const checkExistingUser = await User.findOne({email: req.body.email})
+            if(!checkExistingUser){
+                const hashPassword = await bcrypt.hash(req.body.password, 10)
+                const newUser = await User.createOne({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: hashPassword
+                })
+                return res.status(200).json({response: "Welcome, signed up successfully"})
+            }
+            else{
+                return res.status(409).json({response: "email already exists"})
+            }
+        }
+    }catch{
+        return res.status(500).json({response: "internal server error"})
+    }
+}
+
+//* login
+async function handleLogin(req, res){
+    try{
+        if(!req.body || !req.body.email || !req.body.password){
+            return res.status(400).json({response: "enter valid details"})
+        }
+        else{
+            const existingUser = await User.findOne({email: req.body.email})
+            if(existingUser){
+                const checkPassword = await bcrypt.compare(req.body.password, existingUser.password)
+                if(checkPassword){
+                    return res.status(200).json({response: "Welcome"})
+                }
+                else{
+                    return res.status(400).json({response: "enter valid password"})
+                }
+            }else{
+                return res.status(404).json({response: "email doesn't exist"})
+            }
+        }
+    }catch(error){
+        return res.status(500).json({response: "internal server error"})
+    }
+}
+
+module.exports = {
+    handleSignUp,
+    handleLogin
+}
---
+++ model/userModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose")
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        minlength: 6,
+        required: true
+    },
+    refreshToken: {
+        type: String
+    }
+}, {timestamp: true})
+
+const User = require("user", userSchema)
+
+module.exports = User
